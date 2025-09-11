df63aadda9552c82819cdd95dcbd18eb30bb362d
st56 <@gmail.com>
2025-09-11 13:23:26 +0300

    добавлено больше тестов LoginUser

--- MoqTesting/UnitTestProject/TAuthService.cs
+++ MoqTesting/UnitTestProject/TAuthService.cs
@@ -9,25 +9,48 @@ namespace UnitTestProject
     public class TAuthService
     {
         [TestMethod]
-        public void TestLoginUser_Valid_Credentials()
+        [DataRow("pavel", "1234")]
+        [DataRow("sasha", "sasha1#$2")]
+        [DataRow("admin", "admin123")]
+        public void TestLoginUserr_Valid_Credentials(string username, string password)
         {
             var mock = new Mock<IUserRepository>();
 
             mock
-                .Setup(r => r.LoginUser("pavel", "1234"))
-                .Returns(new User { username = "pavel", password = "1234"});
-
-            mock
-                .Setup(r => r.LoginUser("sasha", "sasha1#$2"))
-                .Returns(new User { username = "sasha", password = "sasha1#$2" });
+                .Setup(r => r.LoginUser(username, password))
+                .Returns(new User { username = username, password = password });
 
             var service = new AuthService(mock.Object);
 
-            bool isLogin = service.LoginUser("pavel", "1234");
+            bool isLogin = service.LoginUser(username, password);
 
             Assert.IsTrue(isLogin);
 
-            mock.Verify(r => r.LoginUser("pavel", "1234"), Times.Once);
+            mock.Verify(r => r.LoginUser(username, password), Times.Once);
+        }
+
+        [TestMethod]
+        [DataRow("pavel", "invalid")]
+        [DataRow("invalid", "1234")]
+        [DataRow("", "")]
+        [DataRow(null, "1234")]
+        [DataRow(null, null)]
+        [DataRow("1234", null)]
+        public void TestLoginUser_Invalid_Credentials(string username, string password)
+        {
+            var mock = new Mock<IUserRepository>();
+
+            mock
+                .Setup(r => r.LoginUser(username, password))
+                .Returns((User)null);
+
+            var service = new AuthService(mock.Object);
+
+            bool isLogin = service.LoginUser(username, password);
+
+            Assert.IsFalse(isLogin);
+
+            mock.Verify(r => r.LoginUser(username, password), Times.Once);
         }
     }
 }
