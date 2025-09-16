d085cbcf421f0bb65169fe8e48616dd4443b7372
st56 <@gmail.com>
2025-09-16 13:20:13 +0300

    добавлен проект winForms

---
+++ RegressionTesting/CommentsWinForms/MainForm.Designer.cs
@@ -0,0 +1,113 @@
+﻿namespace CommentsWinForms
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.commentTextBox = new System.Windows.Forms.Label();
+            this.userLabel = new System.Windows.Forms.Label();
+            this.userComboBox = new System.Windows.Forms.ComboBox();
+            this.addCommentButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox1.Location = new System.Drawing.Point(15, 35);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(347, 21);
+            this.textBox1.TabIndex = 0;
+            // 
+            // commentTextBox
+            // 
+            this.commentTextBox.AutoSize = true;
+            this.commentTextBox.BackColor = System.Drawing.SystemColors.Control;
+            this.commentTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.commentTextBox.Location = new System.Drawing.Point(12, 16);
+            this.commentTextBox.Name = "commentTextBox";
+            this.commentTextBox.Size = new System.Drawing.Size(99, 16);
+            this.commentTextBox.TabIndex = 1;
+            this.commentTextBox.Text = "Комментарий:";
+            // 
+            // userLabel
+            // 
+            this.userLabel.AutoSize = true;
+            this.userLabel.BackColor = System.Drawing.SystemColors.Control;
+            this.userLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.userLabel.Location = new System.Drawing.Point(12, 65);
+            this.userLabel.Name = "userLabel";
+            this.userLabel.Size = new System.Drawing.Size(105, 16);
+            this.userLabel.TabIndex = 2;
+            this.userLabel.Text = "Пользователь:";
+            // 
+            // userComboBox
+            // 
+            this.userComboBox.DisplayMember = "fewf";
+            this.userComboBox.FormattingEnabled = true;
+            this.userComboBox.Location = new System.Drawing.Point(15, 84);
+            this.userComboBox.Name = "userComboBox";
+            this.userComboBox.Size = new System.Drawing.Size(157, 21);
+            this.userComboBox.TabIndex = 3;
+            // 
+            // addCommentButton
+            // 
+            this.addCommentButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.addCommentButton.Location = new System.Drawing.Point(178, 65);
+            this.addCommentButton.Name = "addCommentButton";
+            this.addCommentButton.Size = new System.Drawing.Size(184, 40);
+            this.addCommentButton.TabIndex = 4;
+            this.addCommentButton.Text = "Добавить комментарий";
+            this.addCommentButton.UseVisualStyleBackColor = true;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(374, 486);
+            this.Controls.Add(this.addCommentButton);
+            this.Controls.Add(this.userComboBox);
+            this.Controls.Add(this.userLabel);
+            this.Controls.Add(this.commentTextBox);
+            this.Controls.Add(this.textBox1);
+            this.Name = "MainForm";
+            this.Text = "Комментарии пользователей";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label commentTextBox;
+        private System.Windows.Forms.Label userLabel;
+        private System.Windows.Forms.ComboBox userComboBox;
+        private System.Windows.Forms.Button addCommentButton;
+    }
+}
+
---
+++ RegressionTesting/CommentsWinForms/MainForm.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using System.Windows.Forms;
+using RegressionTesting;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace CommentsWinForms
+{
+    public partial class MainForm: Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+
+            FillUsers();
+        }
+
+        public void FillUsers()
+        {
+            List<User> users = new List<User>
+            {
+                new User { Id = 1, Username = "John" },
+                new User { Id = 2, Username = "Alice" },
+                new User { Id = 3, Username = "Bob" }
+            };
+
+            userComboBox.DataSource = null;
+            userComboBox.Items.Clear();
+
+            userComboBox.DataSource = users;
+            userComboBox.DisplayMember = "Username";
+        }
+    }
+}
